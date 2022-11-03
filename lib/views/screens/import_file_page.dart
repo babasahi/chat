28d4d3476274c8f740c74be2a1cc70c1697f0876
babe-saleh-mahfoud <babesalehmahfoud@gmail.com>
2022-11-03 19:35:51 +0100

    hcanges

--- lib/views/screens/import_file_page.dart
+++ lib/views/screens/import_file_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat/constants.dart';
 import 'package:flutter/material.dart';
 
 class ImportFilePage extends StatelessWidget {
@@ -8,16 +9,25 @@ class ImportFilePage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: GestureDetector(
-            onTap: () async {
-              importFiles();
-            },
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.all(Radius.circular(12)),
+          child: Material(
+            borderRadius: const BorderRadius.all(Radius.circular(12)),
+            elevation: 3,
+            child: GestureDetector(
+              onTap: () async {
+                importFiles();
+              },
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 12, horizontal: 6),
+                decoration: const BoxDecoration(
+                  color: Colors.blueAccent,
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                ),
+                child: Text(
+                  'Import Files',
+                  style: kMessageTextStyle.copyWith(fontSize: 24),
+                ),
               ),
-              child: const Text('Import Files'),
             ),
           ),
         ),
