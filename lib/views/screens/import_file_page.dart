186d894721cdf11f8fb366c12d72514c2a249604
babe-saleh-mahfoud <babesalehmahfoud@gmail.com>
2022-11-03 17:34:31 +0100

    added import file page

--- lib/views/screens/import_file_page.dart
+++ lib/views/screens/import_file_page.dart
@@ -2,20 +2,26 @@ import 'package:flutter/material.dart';
 
 class ImportFilePage extends StatelessWidget {
   const ImportFilePage({super.key});
-
+  Future<void> importFiles() async {}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Center(
-        child: Container(
-          decoration: const BoxDecoration(
-            color: Colors.blueAccent,
-            borderRadius: BorderRadius.all(Radius.circular(12)),
+        child: Center(
+          child: GestureDetector(
+            onTap: () async {
+              importFiles();
+            },
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+              ),
+              child: const Text('Import Files'),
+            ),
           ),
-          child: const Text('Import Files'),
         ),
-      )),
+      ),
     );
   }
 }
